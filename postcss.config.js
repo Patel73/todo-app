53efb80a861542d33201f9ba1599caf8160483fa
Patel73 <nadimkhanpatel@gmail.com>
2025-08-28 00:12:42 +0530

    fix: tailwind and vite config for production build

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,3 @@
-// // export default {
-// //   plugins: {
-// //     tailwindcss: {},
-// //     autoprefixer: {},
-// //   },
-// // }
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from 'tailwindcss'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   css: {
-//     postcss: {
-//       plugins: [tailwindcss()],
-//     },
-//   },
-// })
-
-
-
 import tailwind from "@tailwindcss/postcss";
 
 export default {
